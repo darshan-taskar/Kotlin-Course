08b021de8450ddb67bdc353b04f637e5c9218964
darshan-taskar <programmingbeast22@gmail.com>
2022-03-07 19:17:17 +0530

    Taking input from user

---
+++ InputOutput.kt
@@ -0,0 +1,12 @@
+import java.util.*;
+fun main(args:Array<String>):Unit{
+    println("Enter the first number")
+    var sc = Scanner(System.`in`)
+    var num1 = sc.nextInt()
+
+    println("Enter the second Number")
+    var num2 = sc.nextInt()
+    var addition = num1 + num2
+
+    println("Addition is :" + addition)
+}
